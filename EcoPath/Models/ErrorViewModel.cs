15e72cfa219bc05d039474c651d1271965433c00
Filote Alexandru-Ionut <filote.i@yahoo.com>
2026-02-11 10:24:32 +0200

    Initial setup

---
+++ EcoPath/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcoPath.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
